5b031b8dc8bcaa32f5d9cf809f7be2f3602e5dbf
Trinaath <129047410+Trinaath21@users.noreply.github.com>
2024-11-26 02:04:44 +0800

    Update api.php

--- pawmatchbackend/routes/api.php
+++ pawmatchbackend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\LostFoundController;
+use App\Http\Controllers\AdoptionPostController;
+use App\Http\Controllers\AdoptionApplicationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +12,21 @@ Route::get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function () {
 
 });
+
+//lost-found
 Route::post('/addlost-pets', [LostFoundController::class, 'store']);
 Route::post('/deleteLostReport', [LostFoundController::class, 'deleteLostReport']);
 Route::post('/getAllReportsByUserID', [LostFoundController::class, 'getAllReportsByUserID']);
 Route::post('/editLostReport', [LostFoundController::class, 'updateReport']);
 Route::post('/getSpecificReport', [LostFoundController::class, 'getSpecificReport']);
 Route::post('/getAllReports', [LostFoundController::class, 'getAllReports']);
+
+//adoption
+Route::post('/adoption-posts', [AdoptionPostController::class, 'store']);
+Route::get('/adoption-posts/{id}', [AdoptionPostController::class, 'show']);
+Route::post('/adoption-posts/{id}', [AdoptionPostController::class, 'update']);
+Route::get('/adoption-posts', [AdoptionPostController::class, 'getPostsByUser']);
+Route::post('/deleteAdoptionPost', [AdoptionPostController::class, 'deleteAdoptionPost']);
+Route::post('/apply-adoption', [AdoptionApplicationController::class, 'store']);
+
+//
