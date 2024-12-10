c39dc6676f76f8dcd6b2c4e5cec73988652d3bad
TAN HUI QUN <124640657+huiqun1027@users.noreply.github.com>
2024-12-10 22:05:22 +0800

    Update web.php for forum

--- pawmatchbackend/routes/web.php
+++ pawmatchbackend/routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ForumPostController;
+use App\Http\Controllers\CommentController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/posts', [ForumPostController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::get('/posts', [ForumPostController::class, 'index']);
+Route::get('/posts/{post_id}', [ForumPostController::class, 'show']);
+Route::post('/posts/{post_id}', [ForumPostController::class, 'update']);
+Route::get('/user/posts/{user_id}', [ForumPostController::class, 'getUserPosts']);
+Route::delete('/posts/{post_id}', [ForumPostController::class, 'destroy']);
+Route::get('/posts/{post_id}/comments', [ForumPostController::class,'getComments']);
+Route::get('posts/{post_id}/comments', [CommentController::class, 'index']);
+Route::post('comments', [CommentController::class, 'store']);
+
+
