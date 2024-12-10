9ae7d63570ad0ca35dc09d23a5bcf4d862fb4bf5
TAN HUI QUN <124640657+huiqun1027@users.noreply.github.com>
2024-12-10 22:03:05 +0800

    Update api.php for forum

--- pawmatchbackend/routes/api.php
+++ pawmatchbackend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\LostFoundController;
+use App\Http\Controllers\ForumPostController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +25,14 @@ Route::post('/addReplyReport', [LostFoundController::class, 'addReplyReport']);
 Route::post('/getReplyReportsByReportID', [LostFoundController::class, 'getReplyReportsByReportID']);
 Route::post('/approveReplyReport', [LostFoundController::class, 'approveReplyReport']);
 Route::post('/rejectReplyReport', [LostFoundController::class, 'rejectReplyReport']);
+
+
+Route::post('/posts', [ForumPostController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::get('/posts', [ForumPostController::class, 'index']);
+Route::get('/posts/{post_id}', [ForumPostController::class, 'show']);
+Route::post('/posts/{post_id}', [ForumPostController::class, 'update']);
+Route::get('/user/posts/{user_id}', [ForumPostController::class, 'getUserPosts']);
+Route::delete('/posts/{post_id}', [ForumPostController::class, 'destroy']);
+Route::get('/posts/{post_id}/comments', [ForumPostController::class,'getComments']);
+Route::get('posts/{post_id}/comments', [CommentController::class, 'index']);
+Route::post('comments', [CommentController::class, 'store']);
